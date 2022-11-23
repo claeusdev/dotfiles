7027f2554146e4d2ef043ba420746a739c057705
Nana Adjei Manu <claeusdev@gmail.com>
2022-11-23 09:58:33 +0100

    chore: try to get formatting back

--- .config/nvim/after/plugin/null-ls.rc.lua
+++ .config/nvim/after/plugin/null-ls.rc.lua
@@ -5,7 +5,6 @@ end
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
   on_attach = function(client, bufnr)
@@ -15,21 +14,21 @@ null_ls.setup({
       vim.api.nvim_command [[autocmd * BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
       vim.api.nvim_command [[augroup END]]
     end
-    if client.supports_method("textDocument/formatting") then
-     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr}) 
-     vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function ()
-          vim.lsp.buf.format({
-            filter = function (current_client)
-              return current_client.name == "null-ls"
-            end,
-            bufnr = bufnr
-          })
-        end
-      })
-    end
+    -- if client.supports_method("textDocument/formatting") then
+    --  vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr}) 
+    --  vim.api.nvim_create_autocmd("BufWritePre", {
+    --     group = augroup,
+    --     buffer = bufnr,
+    --     callback = function ()
+    --       vim.lsp.buf.format({
+    --         filter = function (current_client)
+    --           return current_client.name == "null-ls"
+    --         end,
+    --         bufnr = bufnr
+    --       })
+    --     end
+    --   })
+    -- end
   end,
   sources = {
     formatting.prettier,
